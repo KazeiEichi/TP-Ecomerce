bda2464e503a83bb8d4c749e494a86f10d104bd7
Tiago Silva <esteprowork@gmail.com>
2019-04-11 11:35:33 +0200

    finalisation acceuil

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,16 +4,27 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ProductRepository;
 
 class IndexController extends AbstractController
 {
     /**
      * @Route("/", name="index")
+     * 
      */
-    public function index()
-    {
+    public function index(ProductRepository $repository)
+    {   
+        $products = $repository->findLast4Products();
+        $bestproducts = $repository->findBestScore();
+        $favproducts = $repository->findFavorite();
+                                
+
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
+            'products' => $products,
+            'bestproducts' => $bestproducts,
+            'favproducts' => $favproducts [array_rand($favproducts)]
+
         ]);
     }
 }
